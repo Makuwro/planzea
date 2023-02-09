e262b64d50ec923876ec15114e0b58039468bc08
Christian <christian@makuwro.com>
2023-02-09 10:04:44 -0500

    reset

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import React from "react";
-import Document from "./components/Document/Document";
 
 export default function App() {
 
   return (
     <>
-      <Document />
+      
     </>
   );
 
