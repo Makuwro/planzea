bb9b212f61b5dccda3024c2da7793b67d593e623
Christian <christian@makuwro.com>
2023-04-02 13:24:29 -0400

    distinguish today and date outside month

--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -1,14 +1,15 @@
 import React, { useEffect, useState } from "react";
 import Client from "../../client/Client";
+import styles from "./Calendar.module.css";
 
 export default function Calendar({client}: {client: Client}) {
 
-  const [dayArrays, setDayArrays] = useState<number[][]>([]);
+  const [timeArrays, setTimeArrays] = useState<number[][]>([]);
+  const date = new Date();
 
   useEffect(() => {
 
     // Find the first Sunday of the month, or the last Sunday of last month.
-    const date = new Date();
     const firstDate = new Date(date.getFullYear(), date.getMonth(), 1);
     const selectedDate = new Date(firstDate);
     selectedDate.setDate(selectedDate.getDate() - selectedDate.getDay());
@@ -20,7 +21,7 @@ export default function Calendar({client}: {client: Client}) {
       const dayArray = [];
       for (let day = 0; 7 > day; day++) {
 
-        dayArray.push(selectedDate.getDate());
+        dayArray.push(selectedDate.getTime());
         selectedDate.setDate(selectedDate.getDate() + 1);
 
       }
@@ -29,29 +30,58 @@ export default function Calendar({client}: {client: Client}) {
     }
 
     // Save the array to the state.
-    setDayArrays(newDayArrays);
+    setTimeArrays(newDayArrays);
 
   }, []);
 
+  const [startTime, setStartTime] = useState<number | null>(null);
+  const [selectedTimes, setSelectedTimes] = useState<number[]>([]);
+
   return (
-    <main>
-      <button>{["January", "February", "March", "April"][new Date().getMonth()]} {new Date().getFullYear()}</button>
-      <ul>
+    <main id={styles.calendar}>
+      <button>{["January", "February", "March", "April"][date.getMonth()]} <span>{date.getFullYear()}</span></button>
+      <ul id={styles.labels}>
         {
           ["Su", "Mo", "Tu", "We", "Th", "Fr", "Sa"].map((letter) => <li key={letter}>{letter}</li>)
         }
       </ul>
-      <section>
+      <section id={styles.days}>
         {
-          dayArrays.map((array, index) => (
+          timeArrays.map((array, index) => (
             <ul key={index}>
-              {array.map((number) => (
-                <li key={number}>
-                  <button>
-                    {number}
-                  </button>
-                </li>
-              ))}
+              {array.map((number) => {
+
+                const selectedDate = new Date(number);
+                const isSelectedDateToday = selectedDate.getFullYear() === date.getFullYear() && selectedDate.getMonth() === date.getMonth() && selectedDate.getDate() === date.getDate();
+                const isSelectedDateInMonth = selectedDate.getMonth() === date.getMonth();
+
+                return (
+                  <li key={number}>
+                    <button id={isSelectedDateToday ? styles.today : undefined} className={!isSelectedDateInMonth ? styles.dateOutsideMonth : undefined} onMouseEnter={() => {
+
+                      if (selectedTimes[0]) {
+
+                        setSelectedTimes([...selectedTimes, number]);
+
+                      }
+
+                    }} onMouseDown={() => {
+
+                      if (!startTime) {
+
+                        setStartTime(number);
+
+                      }
+
+                      setSelectedTimes([...selectedTimes, number]);
+
+                    }}>
+                      {selectedDate.getDate()}
+                    </button>
+                  </li>
+                );
+
+              })}
             </ul>
           ))
         }
