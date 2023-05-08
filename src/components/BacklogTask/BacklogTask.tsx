44b19af08f334d0a5dffef73896f5798dc769d42
Christian <christian@makuwro.com>
2023-05-08 12:26:19 -0400

    Add sound effects

--- src/components/BacklogTask/BacklogTask.tsx
+++ src/components/BacklogTask/BacklogTask.tsx
@@ -1,15 +1,22 @@
 import React, { useEffect, useRef, useState } from "react";
 import styles from "./BacklogTask.module.css";
 import Icon from "../Icon/Icon";
+import Project from "../../client/Project";
+import Task from "../../client/Task";
+import Client from "../../client/Client";
+import completeSound from "../../complete.ogg";
 
 interface BacklogTaskComponentProperties {
+  client: Client;
   isSelected: boolean;
-  name: string;
+  task: Task;
+  project: Project;
   onClick: () => void;
   onDelete: () => void;
+  onUpdate: (newTask: Task) => void;
 }
 
-export default function BacklogTask({name, isSelected, onClick, onDelete}: BacklogTaskComponentProperties) {
+export default function BacklogTask({client, task, project, isSelected, onClick, onDelete, onUpdate}: BacklogTaskComponentProperties) {
 
   const [isStatusSelectorOpen, setIsStatusSelectorOpen] = useState<boolean>(false);
   const statusButtonRef = useRef<HTMLButtonElement>(null);
@@ -33,15 +40,40 @@ export default function BacklogTask({name, isSelected, onClick, onDelete}: Backl
 
   }, []);
 
+  const status = project.statuses.find((status) => status.id === task.statusId);
+  const statusHexBG = status ? `#${status.backgroundColor.toString(16)}` : undefined;
+
+  async function setStatus(newStatusId: string) {
+
+    if (task.statusId !== newStatusId) {
+
+      await task.update({statusId: newStatusId});
+      task.statusId = newStatusId;
+      onUpdate(new Task(structuredClone(task), client));
+
+      // Make a sound if the task is completed.
+      if (newStatusId === "dc") {
+
+        const audio = new Audio(completeSound);
+        audio.play();
+
+      }
+
+    }
+
+    setIsStatusSelectorOpen(false);
+
+  }
+
   return (
     <li className={`${styles.task}${isSelected ? ` ${styles.selected}` : ""}`}>
       <button onClick={onClick} />
       <section>
         <span>
           <section className={styles.statusContainer}>
-            <button className={styles.status} onClick={() => setIsStatusSelectorOpen(!isStatusSelectorOpen)} ref={statusButtonRef} />
+            <button className={styles.status} onClick={() => setIsStatusSelectorOpen(!isStatusSelectorOpen)} ref={statusButtonRef} style={{backgroundColor: statusHexBG}} />
           </section>
-          <span>{name}</span>
+          <span style={task.statusId === "dc" ? {color: "#9d9d9d"} : undefined}>{task.name}</span>
         </span>
         <span className={styles.taskOptions}>
           <button onClick={onDelete}>
@@ -52,15 +84,15 @@ export default function BacklogTask({name, isSelected, onClick, onDelete}: Backl
       <section className={`${styles.statusSelectorContainer}${isStatusSelectorOpen ? ` ${styles.open}` : ""}`}>
         <section className={styles.statusSelector} ref={statusSelectorRef}>
           <ul>
-            <li>
-              <button onClick={() => setIsStatusSelectorOpen(false)}>Not Started</button>
-            </li>
-            <li>
-              <button>In Progress</button>
-            </li>
-            <li>
-              <button>Completed</button>
-            </li>
+            {
+              project.statuses.map((status) => (
+                <li key={status.id}>
+                  <button onClick={() => setStatus(status.id)}>
+                    {status.name}
+                  </button>
+                </li>
+              ))
+            }
           </ul>
         </section>
       </section>
