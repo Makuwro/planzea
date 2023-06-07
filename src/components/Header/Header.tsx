1860d49367a9711c9d5c066f905b4a034ee0bb38
Christian Toney <37517470+Sudobeast@users.noreply.github.com>
2023-06-06 23:28:31 -0400

    Rename to Personal

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,25 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styles from "./Header.module.css";
-import Icon from "../Icon/Icon";
 
 export default function Header() {
 
   return (
     <header>
       <section id={styles.left}>
-        {/* <button id={styles.projectSelector}>Personal</button>
-        <nav>
-          <Link to="/personal/projects/tasks" className={styles.selected}>Tasks</Link>
-          <Link to="/calendar">Calendar</Link>
-        </nav> */}
         <Link to={"/"}>
           Projects
         </Link>
       </section>
       <section id={styles.right}>
         <button id={styles.accountButton}>
-          <span id={styles.nameContainer}>Christian Toney</span>
+          <span id={styles.nameContainer}>Personal</span>
           <span id={styles.avatarContainer}>
             
           </span>
