d92372398df5082b8f1fb53d7b9ccff79667b631
Christian Toney <37517470+Sudobeast@users.noreply.github.com>
2023-06-22 19:26:52 -0400

    Set current project to null on home visit

--- src/App.tsx
+++ src/App.tsx
@@ -68,7 +68,7 @@ export default function App() {
         <Route path="/:username/projects/:projectId/tasks/:taskId" element={<Backlog client={client} setCurrentProject={(project) => setCurrentProject(project)} setDocumentTitle={setDocumentTitle} />} />
         <Route path="/:username/projects/:projectId/settings" element={<SettingsPage client={client} project={currentProject} setCurrentProject={setCurrentProject} setDocumentTitle={setDocumentTitle} />} />
         <Route path="/:username/projects/:projectId/settings/labels" element={<SettingsPage client={client} project={currentProject} setCurrentProject={setCurrentProject} setDocumentTitle={setDocumentTitle} />} />
-        <Route path="/" element={<HomePage client={client} setDocumentTitle={setDocumentTitle} />} />
+        <Route path="/" element={<HomePage client={client} setDocumentTitle={setDocumentTitle} setCurrentProject={setCurrentProject} />} />
       </Routes>
     </>
   ) : null;
