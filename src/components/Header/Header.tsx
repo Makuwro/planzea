c09a769148b8f7dd031c2db525b569f4cba584da
Christian Toney <christian@makuwro.com>
2023-06-28 12:28:07 -0400

    Add search bar functionality: search for projects by name (#89)

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,11 +3,10 @@ import { Link } from "react-router-dom";
 import styles from "./Header.module.css";
 import HeaderProjectSwitcher from "../HeaderProjectSwitcher/HeaderProjectSwitcher";
 import Project from "../../client/Project";
-import Client from "../../client/Client";
 import Search from "../Search/Search";
-import UIClient from "../../client/UIClient";
+import CacheClient from "../../client/CacheClient";
 
-export default function Header({client, currentProject, uiClient}: {client: Client; currentProject: Project | null; uiClient: UIClient}) {
+export default function Header({client, currentProject}: {client: CacheClient; currentProject: Project | null}) {
 
   const [leftContainerWidth, setLeftContainerWidth] = useState<number>(195.43);
   const leftContainerRef = useRef<HTMLElement>(null);
@@ -39,7 +38,7 @@ export default function Header({client, currentProject, uiClient}: {client: Clie
         <HeaderProjectSwitcher client={client} currentProject={currentProject} />
       </section>
       <section id={styles.right}>
-        <Search client={client} uiClient={uiClient} onMobileSearchChange={(isMobileSearching) => setIsMobileSearching(isMobileSearching)} />
+        <Search client={client} onMobileSearchChange={(isMobileSearching) => setIsMobileSearching(isMobileSearching)} />
         <button id={styles.accountButton}>
           <span id={styles.nameContainer}>User</span>
           <span id={styles.avatarContainer}>
